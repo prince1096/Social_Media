7bd1684674d5006c50379d372be33596e05565ec
prince1096 <princeraj10121996@gmail.com>
2023-07-07 15:28:44 +0530

    RemoveComments

--- src/Explore/Explore.jsx
+++ src/Explore/Explore.jsx
@@ -1,35 +1,13 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import styles from "./Explore.module.css";
 
-import { BsThreeDots } from "react-icons/bs";
-import { AiOutlineHeart } from "react-icons/ai";
-import { FaRegComment } from "react-icons/fa";
-import { LuShare2 } from "react-icons/lu";
-import { FaRegBookmark } from "react-icons/fa";
-
-import AddPost from "../Componets/AddPost/AddPost";
-import Profile from "../Componets/Profile/Profile";
 import PostDisplay from "../Componets/PostDisplay/PostDisplay";
 import { DataUserContext } from "../Context/DataUser/DataUserProvider";
+import Filter from "../Componets/Filter/Filter";
 
 const Explore = () => {
   const { state } = useContext(DataUserContext);
-
-  // console.log(state?.sortByDate);
-
-  // const sortedDatePost =
-  //   state?.sortByDate === "date"
-  //     ? [...state?.post]?.sort(
-  //         (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-  //       )
-  //     : [...state?.post];
-
-  // const sortedDatePost = [
-  //   ...state?.post?.sort(
-  //     (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
-  //   ),
-  // ];
 
   const sortedDatePost =
     state?.sortByDate === "date"
@@ -39,8 +17,6 @@ const Explore = () => {
           return new Date(b.createdAt) - new Date(a.createdAt);
         })
       : [...state?.post];
-
-  // console.log(sortedDatePost);
 
   const sortedLikePost =
     state?.sortByLike === "like"
@@ -54,9 +30,9 @@ const Explore = () => {
   return (
     <div>
       <div className={styles.container}>
-        {/* <div className={styles.addpost}>
-          <AddPost />
-        </div> */}
+        <div className={styles.filtermobile}>
+          <Filter />
+        </div>
 
         {sortedLikePost?.map((post) => (
           <PostDisplay post={post} key={post._id} />
--- src/Scroll/Scroll.jsx
+++ src/Scroll/Scroll.jsx
@@ -1,12 +1,6 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 
 import styles from "./Scroll.module.css";
-
-import { BsThreeDots } from "react-icons/bs";
-import { AiOutlineHeart } from "react-icons/ai";
-import { FaRegComment } from "react-icons/fa";
-import { LuShare2 } from "react-icons/lu";
-import { FaRegBookmark } from "react-icons/fa";
 
 import AddPost from "../Componets/AddPost/AddPost";
 import Filter from "../Componets/Filter/Filter";
@@ -16,35 +10,26 @@ import { DataUserContext } from "../Context/DataUser/DataUserProvider";
 const Scroll = () => {
   const { state } = useContext(DataUserContext);
 
-  // console.log(state?.sortByDate);
-
-  const userInformation = localStorage.getItem("userInformation");
-  const userData = JSON.parse(userInformation);
-
   const followerPost = state?.post?.filter((userpost) =>
     state?.following?.some((user) => user.username === userpost?.username)
   );
-
-  // console.log(postOnHomePage);
 
   const selfPost = state?.post?.filter(
     (userpost) => userpost?.username === state?.loginUser?.username
   );
 
-  // console.log(state?.loginUser?.username);
-
-  // console.log(selfPost);
-
   const postOnHomePage = [...followerPost, ...selfPost];
+
+  const postedPage = [...postOnHomePage]?.sort((a, b) => {
+    return new Date(b.createdAt) - new Date(a.createdAt);
+  });
 
   const sortedDatePost =
     state?.sortByDate === "date"
-      ? [...postOnHomePage]?.sort((a, b) => {
-          // console.log("a.createdAt:", a.createdAt);
-          // console.log("b.createdAt:", b.createdAt);
+      ? [...postedPage]?.sort((a, b) => {
           return new Date(b.createdAt) - new Date(a.createdAt);
         })
-      : [...postOnHomePage];
+      : [...postedPage];
 
   const sortedLikePost =
     state?.sortByLike === "like"
